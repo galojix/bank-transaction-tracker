65756b7ba43432ab601524c32c439eefdf57a633
Greg Cowell <greg.cowell@gmail.com>
2017-02-16 12:30:51 +1000

    Get Flask to display some data stored in the database

--- web.py
+++ web.py
@@ -1,18 +1,14 @@
-from flask import Flask
-from database_setup import User, session
+from flask import Flask, render_template
+from database_setup import User, Transaction, session
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    users = session.query(User)
-    output = ''
-    for user in users:
-        output += user.username
-        output += '</br>'
-    return output
+@app.route('/home')
+def home_page():
+    transactions = session.query(Transaction).all()
+    return render_template('index.html',transactions=transactions)
 
 if __name__ == '__main__':
     app.debug = True
