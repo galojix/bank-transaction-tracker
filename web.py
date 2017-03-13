b542f401d629487f3a3e41e293a316a021b261df
Greg Cowell <greg.cowell@gmail.com>
2017-03-13 11:48:53 +1000

    Add page not found and internal server error pages Start using Flask-Bootstrap extension

--- web.py
+++ web.py
@@ -246,6 +246,17 @@ def reports_page():
     if not session.get('logged_in'):
         return render_template('login.html')
     return render_template('reports.html',menu="reports")
+    
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+    
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(24) # Required for sessions, used for signing cookies
