fc9b02cf1136f60eb08cdc9778522133e476bba4
Greg Cowell <greg.cowell@gmail.com>
2017-03-16 12:24:37 +1000

    Make all modules PEP8 compliant

--- demo_data.py
+++ demo_data.py
@@ -1,4 +1,4 @@
-from web import User, Business, Category, Account, Transaction, db, empty_database
+from web import User, db, empty_database
 from lib_common import hash_password
 
 # Start with an empty database
@@ -8,29 +8,31 @@ empty_database()
 user = User(username="demo", password=hash_password("demo"))
 
 # Create user's businesses
-businesses = ("Acme", "Wonka", "ABC Corp", "Bupa", "Coles", "Galojix", "Lowes", "Caltex", "Unknown")
+businesses = ("Acme", "Wonka", "ABC Corp", "Bupa", "Coles", "Galojix", "Lowes",
+              "Caltex", "Unknown")
 for business in businesses:
     user.add_business(busname=business)
 
 # Create user's categories
-categories = ( ("Food", "Expense"), 
-               ("Entertainment", "Expense"), 
-               ("Insurance", "Expense"), 
-               ("Unspecified Expense", "Expense"),
-               ("Unspecified Income", "Income")  )
+categories = (("Food", "Expense"),
+              ("Entertainment", "Expense"),
+              ("Insurance", "Expense"),
+              ("Unspecified Expense", "Expense"),
+              ("Unspecified Income", "Income"))
 for catname, cattype in categories:
     user.add_category(catname=catname, cattype=cattype)
 
 # Create user's accounts
-accounts = ( ("NAB", 50000), ("ANZ", 20000), ("Unknown", 0) )
+accounts = (("NAB", 50000), ("ANZ", 20000), ("Unknown", 0))
 for accname, balance in accounts:
     user.add_account(accname=accname, balance=balance)
 
-# Create user's transactions    
-transactions = ( (7007, "2016-10-01T08:05", "Bupa", "Insurance", "NAB"), 
-                 (6007, "2016-10-01T08:06", "ABC Corp", "Entertainment", "NAB") )  
+# Create user's transactions
+transactions = ((7007, "2016-10-01T08:05", "Bupa", "Insurance", "NAB"),
+                (6007, "2016-10-01T08:06", "ABC Corp", "Entertainment", "NAB"))
 for amount, date, busname, catname, accname in transactions:
-    user.add_transaction(amount=amount, date=date, busname=busname, catname=catname, accname=accname)
+    user.add_transaction(amount=amount, date=date, busname=busname,
+                         catname=catname, accname=accname)
 
 db.session.add(user)
 db.session.commit()
@@ -39,29 +41,31 @@ db.session.commit()
 user = User(username="normal", password=hash_password("normal"))
 
 # Create user's businesses
-businesses = ("Acme", "Wonka", "ABC Corp", "Bupa", "Coles", "Galojix", "Lowes", "Caltex", "Unknown")
+businesses = ("Acme", "Wonka", "ABC Corp", "Bupa", "Coles", "Galojix", "Lowes",
+              "Caltex", "Unknown")
 for business in businesses:
     user.add_business(busname=business)
 
 # Create user's categories
-categories = ( ("Food", "Expense"), 
-               ("Entertainment", "Expense"), 
-               ("Insurance", "Expense"), 
-               ("Unspecified Expense", "Expense"),
-               ("Unspecified Income", "Income")  )
+categories = (("Food", "Expense"),
+              ("Entertainment", "Expense"),
+              ("Insurance", "Expense"),
+              ("Unspecified Expense", "Expense"),
+              ("Unspecified Income", "Income"))
 for catname, cattype in categories:
     user.add_category(catname=catname, cattype=cattype)
 
 # Create user's accounts
-accounts = ( ("NAB", 50000), ("ANZ", 20000), ("Unknown", 0) )
+accounts = (("NAB", 50000), ("ANZ", 20000), ("Unknown", 0))
 for accname, balance in accounts:
     user.add_account(accname=accname, balance=balance)
 
-# Create user's transactions    
-transactions = ( (8007, "2016-10-01T08:05", "Bupa", "Insurance", "NAB"), 
-                 (9007, "2016-10-01T08:06", "ABC Corp", "Entertainment", "NAB") )  
+# Create user's transactions
+transactions = ((8007, "2016-10-01T08:05", "Bupa", "Insurance", "NAB"),
+                (9007, "2016-10-01T08:06", "ABC Corp", "Entertainment", "NAB"))
 for amount, date, busname, catname, accname in transactions:
-    user.add_transaction(amount=amount, date=date, busname=busname, catname=catname, accname=accname)
+    user.add_transaction(amount=amount, date=date, busname=busname,
+                         catname=catname, accname=accname)
 
 db.session.add(user)
 db.session.commit()
