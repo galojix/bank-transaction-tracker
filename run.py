3ae7875791c467afc992f886bfa8e605db9e1275
Greg Cowell <greg.cowell@gmail.com>
2017-05-29 12:59:51 +1000

    Add some basic unit testing

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate, MigrateCommand
 import os
 from pft import create_app
 from pft.database import db, User, Business, Category, Account, Transaction
+import unittest
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -19,6 +20,12 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
---
+++ tests/test_basics.py
@@ -0,0 +1,30 @@
+"""Basic Unit Tests."""
+import unittest
+from flask import current_app
+from pft import create_app
+from pft.database import db
+
+
+class BasicsTestCase(unittest.TestCase):
+    """Basic Test Case."""
+
+    def setUp(self):
+        """Set up tests."""
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        """Clean up after tests."""
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        """Test app exists."""
+        self.assertFalse(current_app is None)
+
+    def test_app_is_testing(self):
+        """Test app is testing."""
+        self.assertTrue(current_app.config['TESTING'])
