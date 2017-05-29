d7f5da9cce78cec57720bff37e0c21604d0b93d1
Greg Cowell <greg.cowell@gmail.com>
2017-05-29 13:35:51 +1000

    Add unit tests for User

---
+++ pft/tests/test_users.py
@@ -0,0 +1,44 @@
+"""User Model Tests."""
+import unittest
+from .. import create_app, db
+from ..database import User
+
+
+class UserModelTestCase(unittest.TestCase):
+    """User model tests."""
+
+    def setUp(self):
+        """Set up tests."""
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        """Clean up after tests."""
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_password_setter(self):
+        """Test password is hased and set."""
+        u = User(password='cat')
+        self.assertTrue(u.password_hash is not None)
+
+    def test_no_password_getter(self):
+        """Test can not get password."""
+        u = User(password='cat')
+        with self.assertRaises(AttributeError):
+            u.password
+
+    def test_password_verification(self):
+        """Test password verification."""
+        u = User(password='cat')
+        self.assertTrue(u.verify_password('cat'))
+        self.assertFalse(u.verify_password('dog'))
+
+    def test_password_salts_are_random(self):
+        """Test password salts are random."""
+        u = User(password='cat')
+        u2 = User(password='cat')
+        self.assertTrue(u.password_hash != u2.password_hash)
