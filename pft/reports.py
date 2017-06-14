cc792eb99c93d8eb3d1bc80821f680a1ab7ff65b
Greg Cowell <greg.cowell@gmail.com>
2017-06-14 13:01:21 +1000

    Change report on navigation bar to a dropdown

--- pft/reports.py
+++ pft/reports.py
@@ -3,9 +3,9 @@ from bokeh.plotting import figure
 from bokeh.embed import components
 
 
-def graph():
+def graph(report_name):
     """Demo graph."""
-    plot = figure(title="Account Balance", x_axis_label='Date',
+    plot = figure(title=report_name, x_axis_label='Date',
                   y_axis_label='Amount')
     plot.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=2)
     script, div = components(plot)
