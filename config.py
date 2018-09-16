93589f53ec41908066709fce1956958beec5e16b
Greg Cowell <greg.cowell@gmail.com>
2018-09-16 12:29:37 +1000

    Change how environment file works

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 """Module that initialises configuration."""
 import os
-
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config:
