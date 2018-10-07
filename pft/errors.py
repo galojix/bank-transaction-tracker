e42f77d374bab66fb1a90322c3b36c8f75f2499c
Greg Cowell <greg.cowell@gmail.com>
2018-10-08 08:58:30 +1000

    Add database rollback to error handler

--- pft/errors.py
+++ pft/errors.py
@@ -1,5 +1,6 @@
 """Module that contains error handlers."""
 from flask import render_template, Blueprint
+from .database import db
 
 error = Blueprint('error', __name__)
 
@@ -13,4 +14,5 @@ def page_not_found(e):
 @error.app_errorhandler(500)
 def internal_server_error(e):
     """Return internal server error HTML page."""
+    db.session.rollback()
     return render_template('500.html'), 500
